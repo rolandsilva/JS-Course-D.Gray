ebe92aa9158a118df69d25e519b67139cbfee12d
Roland Silva <roland.silva@me.com>
2025-04-02 15:06:22 -0400

    JS course  D.Gray  cont....

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// console.log("Hello World");
+// console.log(typeof "Roland");
+// console.log(typeof 42);
+// console.log(typeof true);
+
+const myVariable = "Mathematics";
+// length property
+console.log(myVariable.length);
+console.log("Every good boy does fine".length);
+
+// string methods
+// charAt() starts at position 0 not 1
+console.log(myVariable.charAt(0));
+console.log(myVariable.charAt(6));
+// indexOf() starts at position 0 not 1
+console.log(myVariable.indexOf("Mat"));
+console.log(myVariable.indexOf("mat"));
